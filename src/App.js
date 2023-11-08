71c77dc19589651c405cb1b9e2e5f902921e1e3c
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-11-08 16:28:17 +0100

    update public/index.html, App.js, list.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,86 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 import { list } from './Data/list';
 
 function App() {
+  const [categories, setCategories] = useState(list);
+  const newCategoryInput = useRef(null);
+
+  const handleAddCategory = () => {
+    const newCategoryTitle = newCategoryInput.current.value;
+    if (newCategoryTitle) {
+      const newCategory = {
+        title: newCategoryTitle,
+        subcategory: []
+      };
+
+      setCategories([...categories, newCategory]);
+      newCategoryInput.current.value = '';
+    }
+  };
+
+  const handleAddSubcategory = (categoryIndex) => {
+    const subcategoryTitle = prompt('Enter the subcategory title:');
+    if (subcategoryTitle) {
+      const updatedCategories = [...categories];
+      updatedCategories[categoryIndex].subcategory.push({
+        title: subcategoryTitle,
+        items: [],
+      });
+      setCategories(updatedCategories);
+    }
+  };
+
+  const handleAddItem = (categoryIndex, subcategoryIndex) => {
+    const itemTitle = prompt('Enter the item title:');
+    if (itemTitle) {
+      const updatedCategories = [...categories];
+      updatedCategories[categoryIndex].subcategory[subcategoryIndex].items.push({
+        title: itemTitle,
+      });
+      setCategories(updatedCategories);
+    }
+  };
+
+  const handleDeleteItem = (categoryIndex, subcategoryIndex, itemIndex) => {
+    const updatedCategories = [...categories];
+    updatedCategories[categoryIndex].subcategory[subcategoryIndex].items.splice(itemIndex, 1);
+    setCategories(updatedCategories);
+  };
+
   return (
     <div className="App">
       <ul className='category'>
-        {list.map((category) => (
-          <li className='category-item'>{category.title}
+        {categories.map((category, categoryIndex) => (
+          <li className='category-item' key={categoryIndex}>
+            {category.title}
             <ul className='subcategory'>
-              {category.subcategory && category.subcategory.map((subcategoryItem) => (
-                <li className='subcategory-item'>
+              {category.subcategory && category.subcategory.map((subcategoryItem, subcategoryIndex) => (
+                <li className='subcategory-item' key={subcategoryIndex}>
                   {subcategoryItem.title}
                   <ul className='items'>
-                    {subcategoryItem.items && subcategoryItem.items.map((item) => (
-                      <li className='items-item'>{item.title}</li>
+                    {subcategoryItem.items && subcategoryItem.items.map((item, itemIndex) => (
+                      <li className='items-item' key={itemIndex}>
+                        {item.title}
+                        <button onClick={() => handleDeleteItem(categoryIndex, subcategoryIndex, itemIndex)}>Delete</button>
+                      </li>
                     ))}
                   </ul>
+                  <button onClick={() => handleAddItem(categoryIndex, subcategoryIndex)}>+</button>
                 </li>
               ))}
             </ul>
+            <button onClick={() => handleAddSubcategory(categoryIndex)}>+</button>
           </li>
         ))}
       </ul>
-    </div >
+
+      <input type="text" ref={newCategoryInput} />
+      <button onClick={handleAddCategory}>+</button>
+    </div>
   );
 }
 
 export default App;
+
+
